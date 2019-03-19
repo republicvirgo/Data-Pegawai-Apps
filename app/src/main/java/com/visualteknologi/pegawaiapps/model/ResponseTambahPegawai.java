cadcae0d914056c8df7c30243f8dd6b055c6a5d7
republicvirgo <republicvirgo@gmail.com>
2019-03-19 09:15:17 +0700

    Feature Tambah Pegawai

---
+++ app/src/main/java/com/visualteknologi/pegawaiapps/model/ResponseTambahPegawai.java
@@ -0,0 +1,37 @@
+package com.visualteknologi.pegawaiapps.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseTambahPegawai {
+
+    @SerializedName("pesan")
+    private String pesan;
+
+    @SerializedName("status")
+    private boolean status;
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "ResponseTambahPegawai{" +
+                        "pesan = '" + pesan + '\'' +
+                        ",status = '" + status + '\'' +
+                        "}";
+    }
+}
---
+++ app/src/main/java/com/visualteknologi/pegawaiapps/network/NetworkClient.java
@@ -0,0 +1,22 @@
+package com.visualteknologi.pegawaiapps.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkClient {
+    public static HttpLoggingInterceptor logging = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    public static OkHttpClient client = new OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build();
+
+    public static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("http://192.168.10.166/server_pegawai/index.php/Api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build();
+
+    public static RestApi service = retrofit.create(RestApi.class);
+}
